a6dc755dbeab16be510ba8e22ea0d5f935490905
Giselle M. Lopes <gisellem767@gmail.com>
2024-05-17 23:39:55 -0300

    Start of index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      screens: {
+        'xs': '365px',
+      },
+      colors: {
+        neutral: '#F8B195',
+        neutralPink: '#F67280',
+        darkPink: '#C06C84',
+        neutralPurple: '#6C5B7B',
+        neutralBlue: '#355C7D',
+      },
+    },
+    plugins: [],
+  }
 }
-
